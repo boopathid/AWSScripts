786c3e3f14ca720fb2dafa785598853a0a77975a
boopathid <boopathid@users.noreply.github.com>
2015-03-24 14:48:53 -0500

    Create ec2_start_instances.sh

---
+++ ec2_start_instances.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/bash
+
+#
+## Setup variables
+#
+. /home/ec2/.profile
+EXC=`cat excluded_instances.log`
+DATE=`date`
+LOGFILE=/home/ec2/ec2instancestatus.log
+LOGFILE2=/home/ec2/ec2instancestatus2.log
+
+#
+## Grab all EBS instances, remove reservations, instance-store, blockdevice.
+## Then print only the instance id's. Finally grep out the excluded instance id's
+## and provide a valid list of them to stop or start.
+#
+EC2INSTANCES=`/home/ec2/bin/ec2-describe-instances | grep -v 'RESERVATION' | grep -v 'instance-store' | grep -v 'BLOCKDEVICE' | awk '{print $2}' | egrep -v $EXC`
+
+#
+## Execute the stop command using the automated generated list of instances.
+## Date the log file and the status of the process.
+#
+echo "STARTING INSTANCES -- $DATE" >> $LOGFILE
+/home/ec2/bin/ec2-start-instances $EC2INSTANCES >> $LOGFILE
